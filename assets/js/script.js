e5bb49f1a5bc46fb5f284c75d1458a36cab05865
Alex Unku <alex.unku@miceanddice.com>
2021-09-29 16:33:51 +0100

    styling the banner

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,12 +19,14 @@ const page = document.getElementById('page');
 if (window.DeviceMotionEvent && typeof window.DeviceMotionEvent.requestPermission === 'function') {
     const banner = document.createElement('div')
     banner.innerHTML = `<div id="banner">Click here to enable DeviceMotion</div>`
-    banner.onclick = getAccelerometer // You NEED to bind the function into a onClick event. An artificial 'onClick' will NOT work.
+    banner.onclick = getAccelerometer.then(res => {
+        document.getElementById("banner").style.display = "none";
+
+    }) // You NEED to bind the function into a onClick event. An artificial 'onClick' will NOT work.
     document.querySelector('body').appendChild(banner)
 }
 
 function getAccelerometer() {
-    document.getElementById("banner").classList.add("display-none")
     window.DeviceMotionEvent.requestPermission()
         .then(response => {
             if (response === 'granted') {
