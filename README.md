c6e4195dde4c0d30327cb960379742b0201419b5
Alex Unku <alex.unku@miceanddice.com>
2021-09-29 16:49:32 +0100

    finalised the demo and styling

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Hello
+# DeviceMotion Demo (iOS)
+A demo for using DeviceMotion (Accelerometer) on iOS devices. 
+
+Works in Safari on iOS 13+.
+
+## Notes
+In order for the main function — `getAccelerometer()` — to work, it needs to be bound to a real `onClick` event
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,14 @@
 const square1 = document.getElementById('square1');
 const page = document.getElementById('page');
-
-// Check I have access to the device orientation event
-// if (typeof DeviceOrientationEvent['requestPermission'] === 'function') {
-//     DeviceOrientationEvent['requestPermission']()
-//         .then(permissionState => {
-//             if (permissionState === 'granted') {
-//                 window.addEventListener('deviceorientation', updateBackgroundGradient);
-//             }
-//         })
-//         .catch(console.error);
-// } else {
-//     if (window.DeviceOrientationEvent) {
-//         window.addEventListener('deviceorientation', updateBackgroundGradient);
-//     }
-// }
+const bannerText = 'Tap to enable DeviceMotion';
+const bannerInnerHtml = `<div id="banner">${bannerText}</div>`
 
 if (window.DeviceMotionEvent && typeof window.DeviceMotionEvent.requestPermission === 'function') {
-    const banner = document.createElement('div')
-    banner.innerHTML = `<div id="banner">Tap to enable DeviceMotion?</div>`
-    banner.onclick = getAccelerometer // You NEED to bind the function into a onClick event. An artificial 'onClick' will NOT work.
-    document.querySelector('body').appendChild(banner)
+    const banner = document.createElement('div');
+    banner.innerHTML = bannerInnerHtml;
+    // This needs to bind the function to a onClick event. An artificial 'onClick' will not work.
+    banner.onclick = getAccelerometer;
+    document.querySelector('body').appendChild(banner);
 }
 
 function getAccelerometer() {
@@ -29,22 +17,22 @@ function getAccelerometer() {
     window.DeviceMotionEvent.requestPermission()
         .then(response => {
             if (response === 'granted') {
-                console.log('DeviceMotion permissions granted.')
+                console.log('DeviceMotion permissions granted')
                 window.addEventListener('deviceorientation', updateBackgroundGradient);
             } else {
-                console.log('DeviceMotion permissions not granted.')
+                console.log('DeviceMotion permissions not granted')
             }
         })
         .catch(e => console.error(e));
 };
 
-
-
-
-// update the background gradient
+// Update the background gradient
 function updateBackgroundGradient(evt) {
-    // Get gamma value and update the position css custom property to match the gamma property of the device orientation event (gamma is tilting left and right)
-    // - I added 50 to account for the offset.
+     /**
+     * Get gamma value and update the position CSS custom property to match the gamma property of the device orientation event
+     * (gamma is tilting left and right)
+     * I added 50 to account for the offset.
+     **/
     page.style.setProperty('--position', -evt.gamma + 50 +'%');
     square1.style.setProperty('--position', -evt.gamma + 50 +'%');
 
